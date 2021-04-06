5243a538cb011cd8d5c98a533e02c3efe305acd6
dukebao <steven.chang420@gmail.com>
2021-04-06 15:23:18 -0700

    add psudo code for javascript

---
+++ script/script.js
@@ -0,0 +1,51 @@
+/* This piece of javascript handles dark and light mode
+use toggle 
+if toggle on , apply different styling to body 
+
+replace dark mode bottom with light theme buttom 
+
+*/
+
+/* This piece of javascript handles cancel button
+if clicked, 
+note and file_handle object with be hidden 
+*/
+
+/*
+"write notes" 
+revent the changes made from cancel 
+clears the text-area 
+
+if object_hidden == True 
+    object_hidden == False
+else 
+    clear text area 
+*/
+
+/* 
+create object with title=note_one 
+body:"content from text-box"
+*/
+
+/* this piece of javascript handles clicking on saved buttom
+if save is clicked 
+    show alert and ask for title
+    if user_input == None
+        save note with a default name
+    else 
+        append input as title, content in textbox as body
+
+in nav 
+    create a new object 
+
+*/
+
+/*this piece of java script handles when user click on nav 
+if content in nav is clicked 
+    look for object_name in object arry
+    display body in textbox 
+
+
+
+
+*/
